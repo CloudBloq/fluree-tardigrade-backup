df731cdc8f6cdde72ccf9378a5fc30a787f1db09
Ijatuyitemi194@gmail.com <Ijatuyitemi194@gmail.com>
2020-06-17 18:20:41 +0100

    started largefile upload

--- src/FlureeTardigradeWorker/FlureeTardigradeWorker/BackupService.cs
+++ src/FlureeTardigradeWorker/FlureeTardigradeWorker/BackupService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -35,9 +36,9 @@ namespace FlureeTardigradeWorker
             uplink.NET.Models.Access.SetTempDirectory(System.IO.Path.GetTempPath());
             var apiKey = _configuration["TardigradeCredential:ApiKey"];
             var secret = _configuration["TardigradeCredential:Secret"];
-            var satelliteAddress = _configuration["TardigradeCredential:SatelliteAddress"];
-             access = new uplink.NET.Models.Access(apiKey, satelliteAddress, secret);
-             objectService = new ObjectService(access);
+            var satelliteAddress = _configuration["TardigradeCredential:SatelliteAddress"]; 
+            access = new uplink.NET.Models.Access(satelliteAddress, apiKey, secret);
+            objectService = new ObjectService(access);
 
         }
 
@@ -47,28 +48,82 @@ namespace FlureeTardigradeWorker
             var restrictedBucketService = new BucketService(access);
 
             var newBucketName = "flureebucket";
-            var file = await GetFlureeSnapshot("file.mp4");
+            var file = await GetFlureeSnapshot("filemovie");
             var bucket = await restrictedBucketService.GetBucketAsync(newBucketName);
-            using Stream source = File.OpenRead(file.FileName);
-            int chunkSize = 4 * 1024 * 1024;
-            long uploadSize = chunkSize;
-            byte[] buffer = new byte[chunkSize];
-            int bytesRead; 
-            while ((bytesRead = source.Read(buffer, 0, buffer.Length)) > 0)
-            {
-                var uploadOperationRestricted = await objectService.UploadObjectAsync(bucket, file.FileName, new UploadOptions(), buffer, true);
-                uploadOperationRestricted.UploadOperationProgressChanged += UploadOperationRestricted_UploadOperationProgressChanged;
-                uploadOperationRestricted.UploadOperationEnded += UploadOperationRestricted_UploadOperationEnded;
-                await uploadOperationRestricted.StartUploadAsync();
+            using Stream source = file.FileInfo.OpenRead();
+//                long chunkSize = 1024 * 512;
+//                    byte[] bytesToUpload = GetRandomBytes(chunkSize);
+                     // Stream stream = new MemoryStream(bytesToUpload); 
+//            int chunkSize = 4 * 1024 * 1024;
+//            long uploadSize = chunkSize;
+//            byte[] buffer = new byte[chunkSize];
+//            int bytesRead;
+//            var customMetadata = new CustomMetadata();
+//            while ((bytesRead = await source.ReadAsync(buffer, 0, buffer.Length)) > 0)
+//            {
+//                customMetadata.Entries = new List<CustomMetadataEntry>(bytesRead);
+//                    
+//               
+//
+////                uploadOperationRestricted.UploadOperationProgressChanged += UploadOperationRestricted_UploadOperationProgressChanged;
+////                uploadOperationRestricted.UploadOperationEnded += UploadOperationRestricted_UploadOperationEnded;
+////                await uploadOperationRestricted.StartUploadAsync();
+//
+//                var mb = ((float)uploadSize / (float)file.File.Length) * 100;
+//                Console.WriteLine($"{mb} % uploaded");
+//                uploadSize += chunkSize;
+//             
+//                //Console.WriteLine($"{uploadOperationRestricted.BytesSent}");
+//            }
 
-                var mb = ((float)uploadSize / (float)file.File.Length) * 100;
-                Console.WriteLine($"{mb} % uploaded");
-                uploadSize += chunkSize;
-            }
+//            var uploadOperationRestricted = await objectService.UploadObjectChunkedAsync(bucket, file.FileName, new UploadOptions(),customMetadata);
+//            uploadOperationRestricted.WriteBytes(buffer);
+//            uploadOperationRestricted.Commit();
+//            Console.WriteLine(uploadOperationRestricted.ErrorMessage);
+
+            var uploadOperationRestricted = await objectService.UploadObjectAsync(bucket, file.FileName, new UploadOptions(),source, true);
+            uploadOperationRestricted.UploadOperationProgressChanged += UploadOperationRestricted_UploadOperationProgressChanged;
+            uploadOperationRestricted.UploadOperationEnded += UploadOperationRestricted_UploadOperationEnded;
+            await uploadOperationRestricted.StartUploadAsync();
+            Console.WriteLine($"{uploadOperationRestricted.BytesSent}");
+
+            //var mb = ((float)chunkSize / (float)file.File.Length) * 100;
+            // Console.WriteLine($"{mb} % uploaded");
+
+            await DownloadBytes(bucket, file.FileName);
+//            var objects = await objectService.ListObjectsAsync(bucket, new ListObjectsOptions());
+//
+//            foreach (var obj in objects.Items)
+//            {
+//                //work with the objects
+//                Console.WriteLine($"Found {obj.Key} {obj.SystemMetaData.Created}");
+//
+//               
+//                //await objectService.DeleteObjectAsync(bucket, obj.Path);
+//
+//            }
+
 
             return 0;
 
             #endregion
+        }
+
+        public async Task DownloadBytes(Bucket bucket, string path)
+        {
+
+            var downloadSvc = await objectService.DownloadObjectAsync(bucket, path, new DownloadOptions(),true);
+            downloadSvc.DownloadOperationProgressChanged += DownloadOperation_DownloadOperationProgressChanged;
+            downloadSvc.DownloadOperationEnded += DownloadOperation_DownloadOperationEnded;
+            await downloadSvc.StartDownloadAsync();
+        }
+
+        private static byte[] GetRandomBytes(long length)
+        {
+            byte[] bytes = new byte[length];
+            Random rand = new Random();
+            rand.NextBytes(bytes);
+            return bytes;
         }
 
         public async Task<string> CreateSnapshot(string databaseName)
@@ -95,6 +150,7 @@ namespace FlureeTardigradeWorker
             return new FileObject
             {
                 File = fileByte,
+                FileInfo = fileInfo,
                 FileName = fileInfo.Name
             };
         }
@@ -129,7 +185,7 @@ namespace FlureeTardigradeWorker
             {
                 //work with the objects
                 Console.WriteLine($"Found {obj.Key} {obj.SystemMetaData.Created}");
-
+                await DownloadBytes(bucket, obj.Key);
 
                 //await objectService.DeleteObjectAsync(bucket, obj.Path);
 
@@ -181,6 +237,7 @@ namespace FlureeTardigradeWorker
     {
         public byte[] File { get; set; }
         public string FileName { get; set; }
+        public FileInfo FileInfo { get; set; }
     }
 
     public class CreateSnapshotModel
